7d777369626272e1682ee460900babed6fb334da
tjurado1 <112440175+tjurado25@users.noreply.github.com>
2022-09-06 21:53:01 +0000

    dhwdhdhwdhwdhdwohowdho

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,15 @@
+print(10*5)
+print(10**2)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(10%15) 
+print(10%10)
+print(0%10)
+print(10/15) #this number goes to infinity and doesnt stop at 15 decimal points, last number needs to be rounded up
+rate = float(input("What is the exchange rate for Euro to Dollar?"))
+amount=float(input("Whats the amount of money exchanged?"))
+total=rate*amount
+result=total- 3
+print(result)
