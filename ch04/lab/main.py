55a587969e2d8edd93481a563aeb448b7d61df3c
tjurado1 <112440175+tjurado25@users.noreply.github.com>
2022-10-08 01:59:19 +0000

    need more time

---
+++ ch04/lab/main.py
@@ -0,0 +1,27 @@
+import pygame   
+import random 
+
+pygame.init()
+surface = pygame.display.set_mode((500,500))
+surface.fill("blue")
+color = (255,0,0)
+pygame.draw.rect(surface, color, pygame.Rect(90, 90, 120, 120))
+pygame.display.flip()
+
+
+
+
+
+
+
+    
+
+
+  
+    
+      
+    
+    
+
+
+
